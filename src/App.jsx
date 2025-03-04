67d372f884fc3292f902f6fe20e3ee7c68a18973
Pepe <josepp2508@gmail.com>
2025-03-04 11:01:38 -0600

    [IMP] new products section & new images

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ProductCard from "./components/ProductCard/ProductCard.jsx";
 import HomeBanner from "./components/HomeBanner/HomeBanner.jsx"
+import NewProducts from "./components/NewProducts/NewProducts.jsx";
 import TopBar from "./components/TopBar/TopBar.jsx";
 import NikeShoe from "./assets/nike-joyride.jpg";
 
@@ -13,6 +14,8 @@ function App() {
             <ProductCard image={NikeShoe} title={'Tennis Nike Edicion Especial Temporada 2025 ProMax'} price={'$1299.99'} />
           </section>
         </div>
+
+        <NewProducts />
     </>
   )
 }
---
+++ src/components/NewProducts/NewProducts.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './new-products.css'
+import ProductCard from "../ProductCard/ProductCard";
+
+import NewProduct_01 from "../../assets/new-product-01.jpeg"
+import NewProduct_02 from "../../assets/new-product-02.jpg"
+import NewProduct_03 from "../../assets/new-product-03.jpg"
+
+function NewProducts() {
+    return (
+        <section className="new-products-section">
+            <h3>Nuevos Productos</h3>
+            <div className="products-images">
+                <ProductCard image={NewProduct_01} title={'Tennis Nike Edicion Especial Temporada 2025 ProMax'} price={'$1299.99'} />
+                <ProductCard image={NewProduct_02} title={'Tennis Nike Edicion Especial Temporada 2025 ProMax'} price={'$1299.99'} />
+                <ProductCard image={NewProduct_03} title={'Tennis Nike Edicion Especial Temporada 2025 ProMax'} price={'$1299.99'} />
+            </div>
+        </section>
+    )
+}
+
+export default NewProducts
