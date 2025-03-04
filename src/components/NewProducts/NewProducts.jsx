3c6a4b64d57456397839e5cc2f24dba1da61ce50
Pepe <josepp2508@gmail.com>
2025-03-04 11:38:59 -0600

    [IMP] scroll animation for new products section

--- src/components/NewProducts/NewProducts.jsx
+++ src/components/NewProducts/NewProducts.jsx
@@ -11,9 +11,9 @@ function NewProducts() {
         <section className="new-products-section">
             <h3>Nuevos Productos</h3>
             <div className="products-images">
-                <ProductCard image={NewProduct_01} title={'Tennis Nike Edicion Especial Temporada 2025 ProMax'} price={'$1299.99'} />
-                <ProductCard image={NewProduct_02} title={'Tennis Nike Edicion Especial Temporada 2025 ProMax'} price={'$1299.99'} />
-                <ProductCard image={NewProduct_03} title={'Tennis Nike Edicion Especial Temporada 2025 ProMax'} price={'$1299.99'} />
+                <ProductCard image={NewProduct_01} title={'Temporada 2025 ProMax'} price={'$1299.99'} />
+                <ProductCard image={NewProduct_02} title={'Tennis Nike Edicion Especial'} price={'$1299.99'} />
+                <ProductCard image={NewProduct_03} title={'Tennis Nike ProMax'} price={'$1299.99'} />
             </div>
         </section>
     )
