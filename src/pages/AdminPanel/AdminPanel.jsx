80afa504520acb2d0c3583a13127001a553f415c
Pepe <josepp2508@gmail.com>
2025-03-04 21:41:29 -0600

    [IMP] login folder renamed to Auth

--- src/pages/AdminPanel/AdminPanel.jsx
+++ src/pages/AdminPanel/AdminPanel.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import './admin-panel.css'
+import supabase from '../../lib/helper/supabaseClient'
+
+const { data, error } = await supabase
+  .from('characters')
+  .select()
+
+let fetchUser = async () => {
+    const { data, error } = await supabase.auth.getUser();
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(data.user);
+    }
+}
 
 function AdminPanel() {
     return(
         <>
             <h2>Admin Panel</h2>
+            <div className="panel">
+                <button onClick={fetchUser} >Fetch user</button>
+            </div>
         </>
     )
 }
