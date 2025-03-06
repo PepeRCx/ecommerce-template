340850c1ba8e15d8e39a1a396d052dd9308df935
Pepe <josepp2508@gmail.com>
2025-03-05 23:08:14 -0600

    [IMP] users panel as separate file

--- src/pages/AdminPanel/AdminPanel.jsx
+++ src/pages/AdminPanel/AdminPanel.jsx
@@ -1,72 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import './admin-panel.css'
-import supabase from '../../lib/helper/supabaseClient'
-import DashboardRow from "../../components/DashboardRows/DashboardRow";
-import { Button } from "@mui/material";
+import MuiBottomNav from "../../components/MuiBottomNav";
+import UsersPanel from "./Users/UsersPanel";
 
 function AdminPanel() {
-    const [users, setUsers] = useState([]);
-    const [isAdmin, setIsAdmin] = useState(null);
-    const navigate = useNavigate();
 
-    useEffect(()=> {
-        const checkIfAdmin = async () => {
-            const { data: { user }, error } = await supabase.auth.getUser();
-            if (error || !user) {
-                navigate('/');
-                return;
-            }
-
-            const { data, error: userError } = await supabase
-                .from("users_data")
-                .select("is_admin")
-                .eq("email", user.email)
-                .single();
-            if (userError || !data || !data.is_admin) {
-                navigate('/');
-            } else {
-                setIsAdmin(true);
-            }
-        };
-
-        checkIfAdmin();
-    }, [navigate]);
-
-    const fetchUsers = async () => {
-        const { data, error } = await supabase
-            .from('users_data')
-            .select('email, is_admin')
-        
-        if (error) {
-            console.log(error)
-        } else {
-            setUsers(data)
-        }
-    }
-
-    const getUserLogged = async () => {
-        const { data: { user }, error } = await supabase.auth.getUser();
-
-        if (error) {
-            alert("Error fetching user:" + error.message);
-        } else {
-            alert("Current User: " + user.email);
-        }
-    }
-
-    return isAdmin ? (
+    return (
         <>
             <h2>Admin Panel</h2>
-            <div className="panel">
-                <Button variant="contained" onClick={fetchUsers} sx={{ m: 2, backgroundColor: 'black' }}>Fetch Users</Button>
-                <Button variant="outlined" sx={{ color: 'black' }} color="black" onClick={getUserLogged} >Current User</Button>
-                {users.map((user, index) =>(
-                    <DashboardRow key={index} userEmail={user.email} userRole={user.is_admin}/>
-                ))}
-            </div>
+            <UsersPanel />
+            <MuiBottomNav />
         </>
-    ) : null;
+    )
 }
 
 export default AdminPanel;
