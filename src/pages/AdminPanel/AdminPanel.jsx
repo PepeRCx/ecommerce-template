b8fccdc8a3e5433d9f8f756a50bdf9349d2d3dd9
Pepe <josepp2508@gmail.com>
2025-03-05 19:24:30 -0600

    [IMP] email and user role shows in admin panel

--- src/pages/AdminPanel/AdminPanel.jsx
+++ src/pages/AdminPanel/AdminPanel.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import './admin-panel.css'
 import supabase from '../../lib/helper/supabaseClient'
-
-const { data, error } = await supabase
-  .from('characters')
-  .select()
-
-let fetchUser = async () => {
-    const { data, error } = await supabase.auth.getUser();
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(data.user);
-    }
-}
+import DashboardRow from "../../components/DashboardRows/DashboardRow";
+import { Button } from "@mui/material";
 
 function AdminPanel() {
+    const [users, setUsers] = useState([]);
+
+    const fetchUsers = async () => {
+        const { data, error } = await supabase
+            .from('users_data')
+            .select('email, is_admin')
+        
+        if (error) {
+            console.log(error)
+        } else {
+            console.log(data)
+            setUsers(data)
+        }
+    }
+
     return(
         <>
             <h2>Admin Panel</h2>
             <div className="panel">
-                <button onClick={fetchUser} >Fetch user</button>
+                <Button variant="contained" onClick={fetchUsers}>Fetch Users</Button>
+                <Button variant="contained">Save</Button>
+                {users.map((user, index) =>(
+                    <DashboardRow key={index} userEmail={user.email} userRole={user.is_admin}/>
+                ))}
             </div>
         </>
     )
